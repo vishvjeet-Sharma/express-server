259fc2d39418d50fc4dad6b0647ff272eeb68cad
Vishvjeet-Sharma <vishvjeet.sharma@successive.tech>
2022-01-17 10:10:49 +0530

    Day 10 -Generic Routes Validations

---
+++ src/libs/routes/validationHandler.ts
@@ -0,0 +1,15 @@
+import { checkSchema, validationResult } from 'express-validator/check';
+
+const validationHandler = (validator) => {
+    return [
+        checkSchema(validator),
+        (req, res, next) => {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                next({ message: 'Bad Request', status: 422, error: errors.array() });
+            }
+            next();
+        }
+    ];
+};
+export default validationHandler;
